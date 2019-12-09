2d1f2fe16b9c7d02fc67aea8d4f95865ba7cd326
xiongshao <jerry29221979@gmail.com>
2019-12-09 22:14:58 +0800

    Add Game Project

---
+++ FirstGameProject/Assets/script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region
+    [Header("速度"), Range(0f, 100f)]
+    public float speed = 3.5f;
+    [Header("跳躍高度"), Range(100, 2000)]
+    public int jumpHigh = 300;
+    [Header("是否在地上")]
+    public bool isGround = false;
+    [Header("玩家名稱")]
+    public string userName = "hello";
+    [Header("2D 剛體")]
+    public Rigidbody2D r2d;
+    public Animator ani;
+    [Header("音效區域")]
+    public AudioSource aud;
+    public AudioClip soundDiamond;
+    #endregion
+
+}
